47a819753aa30a20f3d1b0cfae54cb2ae244682e
Alexander Kiel <alexanderkiel@gmx.net>
2016-03-29 20:27:52 +0200

    Update ODM Parser to v0.1

--- project.clj
+++ project.clj
@@ -1,11 +1,11 @@
-(defproject lens-odm-diff "0.1-SNAPSHOT"
+(defproject org.clojars.akiel/lens-odm-diff "0.1-SNAPSHOT"
   :description "Diffs ODM XML files."
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
 
-  :dependencies [[org.clojure/clojure "1.8.0"]
-                 [lens-odm-parser "0.1-SNAPSHOT"]]
+  :dependencies [[org.clojars.akiel/lens-odm-parser "0.1"]
+                 [org.clojure/clojure "1.8.0"]]
 
   :profiles {:dev
              {:source-paths ["dev"]
