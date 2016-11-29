3ad2af26b29aee838cfa642f01e23908a80ce1a0
Alexander Kiel <alexanderkiel@gmx.net>
2016-11-29 17:07:04 +0100

    Be Pedantic About Dependencies

--- project.clj
+++ project.clj
@@ -4,6 +4,9 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
 
+  :min-lein-version "2.0.0"
+  :pedantic? :abort
+
   :dependencies [[org.clojars.akiel/lens-odm-parser "0.2"]
                  [org.clojure/clojure "1.8.0"]]
 
